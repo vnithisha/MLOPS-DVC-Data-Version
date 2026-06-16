dd871785ac56099eac496604d028afe4a373e85f
vnithisha <vaddepallinithisha@gmail.com>
2026-06-16 10:18:51 +0530

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+# create a sample dataframe with column names
+data = {'Name' : ['Alice', 'Bob', 'Charlie'],
+        'Age' : [25, 30, 35],
+        'City' : ['New York', 'Philadee', 'South Korea'],}
+df = pd.DataFrame(data, columns = ['Name', 'Age', 'City'])
+
+# Ensure data directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok = True)
+
+# Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# Save the dataframe to a csv file, including column names
+df.to_csv(file_path, index = False)
+
+print(f"CSV file saved to {file_path}")
